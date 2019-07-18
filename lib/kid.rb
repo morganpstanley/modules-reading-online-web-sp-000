edb7c72454253f0ee1d86b988bb403bec0b9938b
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 03:56:04 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative 'fancy_dance.rb'
 
 class Kid
+
+  attr_accessor :name
   include Dance
   extend MetaDancing
   extend FancyDance::ClassMethods
