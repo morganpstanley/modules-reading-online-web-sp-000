1f340241cd07d0670d1263d880901a340b8c77f5
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 03:58:05 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,14 @@ require_relative './dance_module.rb'
 require_relative 'fancy_dance.rb'
 
 class Dancer
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  include Dance
+  extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,15 @@ require_relative './class_methods_module.rb'
 require_relative 'fancy_dance.rb'
 
 class Kid
+
+    attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  include Dance
+  extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
